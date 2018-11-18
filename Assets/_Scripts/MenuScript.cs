6c94cd0abf69fdbdcaf8d3b84006c904be6b3476
arjunc007 <arjunchauhan007@gmail.com>
2018-11-18 13:58:00 +0000

    Added home screen and connected scenes

---
+++ Assets/_Scripts/MenuScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+    public string gameScene = "Game";
+    public string menuScene = "Main Menu";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(gameScene);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene(menuScene);
+    }
+}
