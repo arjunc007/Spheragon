b3a590086f1ab9b9470e599f859186afd5447e68
arjunc007 <arjunchauhan007@gmail.com>
2019-03-06 01:33:46 +0530

    Updated project to Unity 2018.3

--- Assets/_Scripts/InputManager.cs
+++ Assets/_Scripts/InputManager.cs
@@ -56,7 +56,6 @@ public class InputManager : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        Debug.Log(GameManager.isPaused);
         if (!GameManager.isPaused)
         {
 #if UNITY_ANDROID
