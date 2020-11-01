0c504cd54ed935fbbaf32320302ba094ac26468c
Arjun Chauhan <arjunchauhan007@gmail.com>
2020-11-01 10:50:07 +0530

    Upgrade GoogleMobileAds SDK and AdMob IDs

--- Assets/_Scripts/AdManager.cs
+++ Assets/_Scripts/AdManager.cs
@@ -8,7 +8,7 @@ public class AdManager : MonoBehaviour
     public RectTransform gameUI;
     private BannerView bannerView;
     private readonly string testAdID = "ca-app-pub-3940256099942544/6300978111";
-    private readonly string bannerAdID_android = "ca-app-pub-5106176281120401/7918618705";
+    private readonly string bannerAdID_android = "ca-app-pub-1613567415878541/2030859285";
 
     private void Awake()
     {
@@ -26,30 +26,14 @@ public class AdManager : MonoBehaviour
     {
         InputManager.instance.OrientationEvent += ReloadAd;
 
-#if UNITY_ANDROID
-        string appId = "ca-app-pub-5106176281120401~5994296974";
-#elif UNITY_IPHONE
-            string appId = "ca-app-pub-3940256099942544~1458002511";
-#else
-            string appId = "unexpected_platform";
-#endif
-
         // Initialize the Google Mobile Ads SDK.
-        MobileAds.Initialize(appId);
+        MobileAds.Initialize(initStatus => { });
 
         this.RequestBanner();
     }
 
     private void RequestBanner()
     {
-        //#if UNITY_ANDROID
-        //        string adUnitId = "ca-app-pub-5106176281120401/7918618705";
-        //#elif UNITY_IPHONE
-        //            string adUnitId = "ca-app-pub-3940256099942544/2934735716";
-        //#else
-        //            string adUnitId = "unexpected_platform";
-        //#endif
-
         // Create a 320x50 banner at the top of the screen.
         if (!InputManager.instance.IsPortrait())
         {
@@ -60,7 +44,7 @@ public class AdManager : MonoBehaviour
             gameUI.offsetMax = new Vector2(gameUI.offsetMax.x, -50);
         }
 
-        bannerView = new BannerView(bannerAdID_android, AdSize.Banner, AdPosition.Top);
+        bannerView = new BannerView(testAdID, AdSize.Banner, AdPosition.Top);
 
         // Create an empty ad request.
         //AdRequest request = new AdRequest.Builder().AddTestDevice("61FF8CC5A520CDFF8D2BDE246EF96DCA").Build();
